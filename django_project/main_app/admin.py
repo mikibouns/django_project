77fa835cfa61600ad1250d5d2f9d191632ad77e8
mikibouns <mikibouns@gmail.com>
2019-02-15 00:13:27 +0200

    start project

--- django_project/main_app/admin.py
+++ django_project/main_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (Hotels,
+                     HotelsParamsHotels,
+                     HotelsParams,
+                     RoomsTypes,
+                     RoomsTypesHotels,
+                     Prices,
+                     Places)
 
-# Register your models here.
+
+admin.site.register(Hotels)
+admin.site.register(HotelsParamsHotels)
+admin.site.register(HotelsParams)
+admin.site.register(RoomsTypes)
+admin.site.register(RoomsTypesHotels)
+admin.site.register(Prices)
+admin.site.register(Places)
--- django_project/main_app/models.py
+++ django_project/main_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from ..auth_app.models import AbstractUser
+from auth_app.models import AbstractUser
 
 
 class Places(models.Model):
     '''Топонимы'''
     title = models.CharField(max_length=128)
-    parent = models.ForeignKey()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
     timeZ = models.CharField(max_length=128)
     uuid = models.CharField(max_length=64)
 
@@ -23,22 +23,9 @@ class RoomsTypes(models.Model):
     params = models.TextField()
 
 
-class RoomsTypesHotels(models.Model):
-    '''Виды номеров-Отели Клиента'''
-    title = models.CharField(max_length=128)
-    value = models.DecimalField(default=0, decimal_places=0)
-    params = models.TextField()
-
-
 class HotelsParams(models.Model):
     '''Параметры отелей'''
     title = models.CharField(max_length=1024)
-    params = models.TextField()
-
-
-class HotelsParamsHotels(models.Model):
-    '''Параметры отелей-Отели Клиента'''
-    title = models.CharField(max_length=128)
     params = models.TextField()
 
 
@@ -48,9 +35,30 @@ class Hotels(models.Model):
     place = models.ForeignKey(Places, on_delete=models.CASCADE)
     owner = models.ForeignKey(AbstractUser, on_delete=models.CASCADE)
     price = models.ForeignKey(Prices, on_delete=models.CASCADE)
-    param = models.ManyToManyField()
-    roomstypes = models.ManyToManyField()
+    param = models.ManyToManyField(HotelsParams,
+                                   through=HotelsParamsHotels,
+                                   through_fields=['hotels'])
+    roomstypes = models.ManyToManyField(RoomsTypes,
+                                        through=RoomsTypesHotels,
+                                        through_fields=['room_types'])
     www = models.CharField(max_length=1024)
     volume = models.DecimalField(default=0, decimal_places=0)
     pms = models.CharField(max_length=128)
+    params = models.TextField()
+
+
+class RoomsTypesHotels(models.Model):
+    '''Виды номеров-Отели Клиента'''
+    hotels = models.ForeignKey(Hotels, on_delete=models.CASCADE)
+    room_types = models.ForeignKey(RoomsTypes, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    value = models.DecimalField(default=0, decimal_places=0)
+    params = models.TextField()
+
+
+class HotelsParamsHotels(models.Model):
+    '''Параметры отелей-Отели Клиента'''
+    hotels = models.ForeignKey(Hotels, on_delete=models.CASCADE)
+    room_types = models.ForeignKey(RoomsTypes, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
     params = models.TextField()
