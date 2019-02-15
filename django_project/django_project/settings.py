add6873c15531059a27b111cfe641834cd963e39
mikibouns <mikibouns@gmail.com>
2019-02-15 23:02:17 +0200

    created schema, changed sqlite to MySQL

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db.cnf')
+        },
     }
 }
 
