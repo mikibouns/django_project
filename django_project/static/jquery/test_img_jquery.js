5d4ac432f681d1b1e30b33e9e1f2ce54d21ab538
mikibouns <mikibouns@gmail.com>
2019-04-10 09:29:52 +0200

    created tiles interior and walls

--- django_project/static/jquery/test_img_jquery.js
+++ django_project/static/jquery/test_img_jquery.js
@@ -1,4 +1,10 @@
-$('.thumbnail').click(function()
+$('.wall').click(function()
 {
   $('.interior').css('background-image', 'url(' + $(this).children("img:first").attr('src') + ')');
 });
+
+$('.int').click(function()
+{
+  $('.interior img').attr('src', $(this).children("img:first").attr('src'));
+});
+
