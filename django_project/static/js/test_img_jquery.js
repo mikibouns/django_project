38cf0c9b6327775ce8c69c22e79abdb9bc7ec101
mikibouns <mikibouns@gmail.com>
2019-04-17 17:01:27 +0200

    added lightslider

--- django_project/static/js/test_img_jquery.js
+++ django_project/static/js/test_img_jquery.js
@@ -32,29 +32,9 @@ $('.room').click(function()
 //}());
 
   $(document).ready(function() {
-    $('#slider').lightSlider({
-        item:4,
-        loop:false,
-        slideMove:2,
-        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:600,
-        responsive : [
-            {
-                breakpoint:800,
-                settings: {
-                    item:3,
-                    slideMove:1,
-                    slideMargin:6,
-                  }
-            },
-            {
-                breakpoint:480,
-                settings: {
-                    item:2,
-                    slideMove:1
-                  }
-            }
-        ]
+	$("#content-slider").lightSlider({
+      loop:true,
+      keyPress:true
     });
   });
 
