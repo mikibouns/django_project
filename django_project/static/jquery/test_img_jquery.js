866a6f1e6e671a9b172fe4ec6d54c4c6c9c62c83
mikibouns <mikibouns@gmail.com>
2019-04-17 16:37:23 +0200

    added lightslider

--- django_project/static/jquery/test_img_jquery.js
+++ django_project/static/jquery/test_img_jquery.js
@@ -8,26 +8,22 @@ $('.room').click(function()
   $('#interior_img').attr('src', $(this).children("img:first").attr('src'));
 });
 
-//var tilesNumber;
-//$(window).on('load resize', function(){
-//  var win = $(this); //this = window
-//  if (win.height() >= 820) {
-//    tilesNumber = 2
-//  }
-//  if (win.width() >= 1280) {
-//    tilesNumber = 4
-//  }
-//});
-
 // карусель для обоев
+var tilesNumber = 4;
+$(window).resize(function(){
+  if($(window).width() < 993){
+     tilesNumber = 2;
+  }
+});
+
 (function(){
   $("#carousel_wallpapers .carousel-inner").children("div:first").addClass("active");
   $("#carousel_collections .carousel-inner").children("div:first").addClass("active");
 
   $('#carousel_wallpapers .item').each(function(){
     var itemToClone = $(this);
-
-    for (var i=1;i<4;i++) {
+    alert(tilesNumber);
+    for (var i=1;i<tilesNumber;i++) {
       itemToClone = itemToClone.next();
 
       // wrap around if at end of item collection
@@ -41,3 +37,5 @@ $('.room').click(function()
     }
   });
 }());
+
+// canvas
