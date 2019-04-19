ee7b375121ebf02da8a9fb89caf3381fd0d756bf
mikibouns <mikibouns@gmail.com>
2019-04-19 12:56:26 +0200

    скрипт добавляет интерьер на холст

--- django_project/static/js/test_img_jquery.js
+++ django_project/static/js/test_img_jquery.js
@@ -9,7 +9,17 @@ $('.room').click(function()
 {
   $('#interior_img').attr('src', $(this).children("img:first").attr('src'));
   $('.room img').css('padding', '0px');
-  $(this).children('img').css('padding', '5px');;
+  $(this).children('img').css('padding', '5px');
+
+  var int_canvas = document.getElementById("canvas_interior"),
+    ctx = int_canvas.getContext('2d'),
+    pic = new Image();
+  int_canvas.width = 1910;
+  int_canvas.height = 1910;
+  pic.src = $(this).children("img:first").attr('src'); // Путь к изображению которое необходимо нанести на холст
+  pic.onload = function (){
+    ctx.drawImage(pic, 0, 0);
+  }
 });
 
 $('#collection-slider button').click(function()
@@ -45,7 +55,25 @@ $(document).ready(function() {
     item:3,
     adaptiveHeight:true,
     vertical:true,
-    slideMargin:0,
+    slideMargin:10,
+    responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                    slideMargin:1,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:5,
+                    slideMove:1,
+                    slideMargin:1,
+                  }
+            }
+        ]
   });
 });
 
