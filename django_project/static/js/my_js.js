f2db62834e2acf7c75bb37c68e8a6892c723b0b5
mikibouns <mikibouns@gmail.com>
2019-04-25 15:27:15 +0200

    адаптация обоев для интерьеров

--- django_project/static/js/my_js.js
+++ django_project/static/js/my_js.js
@@ -1,67 +1,52 @@
-// canvas
-function showInterior(interior_pic){
-    var int_canvas = document.getElementById("canvas_interior"),
-        ctx = int_canvas.getContext('2d'),
-        pic1 = new Image();
-    int_canvas.width = 1910;
-    int_canvas.height = 1910;
-    pic1.src = interior_pic; // Путь к изображению интерьера которое необходимо нанести на холст
-    pic1.onload = function(){
-        ctx.drawImage(pic1, 0, 0);
+// global var
+var interior_pic = $('#interior-slider li:first img').attr('src'); // изображение для интерьера
+var wallpaper_pic = $('#wallpaper-slider img:first').attr('src'); // изображение для обоев
 
+
+// заполнение стены обоями
+function fillWall(pic, ctx, rapport, picSize, tileX=0, tileY=0){
+    var tileSizeX = pic.naturalWidth / picSize;
+    var tileSizeY = pic.naturalHeight / picSize;
+    var x = 8;
+    var y = 8;
+    for(tileX; tileX < x; tileX++) {
+        tileY = 0;
+        if (tileX % 2 == 0){
+            tileY -= rapport; // если есть раппорт, смещаем не четную полосу на указанное значение
+        }
+        for(tileY; tileY < y; tileY++) {
+            ctx.drawImage(pic, tileX * tileSizeX,tileY * tileSizeY, tileSizeX, tileSizeY);
+        }
     }
-
-//    var canvas = document.getElementById('canvas_interior');
-//    var ctx = canvas.getContext('2d');
-//    canvas.width = 740;
-//    canvas.height = 740;
-//    ctx.fillStyle = 'red';
-//    ctx.fillRect(100, 100, 150, 75);
-//    ctx.clearRect(0, 0, 1910, 1910);
-//    ctx.rect(500, 500, 250, 250);
-//    ctx.strokeStyle = 'green';
-//    ctx.lineWidth = '10';
-//    ctx.stroke();
-//    ctx.fill();
-//
-//    ctx.beginPath();
-//    ctx.strokeStyle = 'black';
-//    ctx.lineWidth = '20';
-//    ctx.moveTo(200, 50);
-//    ctx.lineTo(300, 50);
-//    ctx.lineCap = 'round';
-//    ctx.stroke();
-//    ctx.closePath();
-//
-//
-//    canvas.onmousedown = function(event){
-//        canvas.onmousemove = function(event){
-//            var x = event.offsetX;
-//            var y = event.offsetY;
-//            ctx.fillRect(x-5, y-5, 10, 10);
-//        }
-//        canvas.onmouseup = function(){
-//            canvas.onmousemove = null;
-//        }
-//    }
 }
 
 
-function fillWall(current_pic){
+// canvas
+function showInterior(interior_pic, wallpaper_pic, rapport=0, picSize=5){
     var int_canvas = document.getElementById("canvas_interior"),
         ctx = int_canvas.getContext('2d'),
-        pic = new Image();
-    pic.src = current_pic;
+        pic1 = new Image(), pic2 = new Image();
     int_canvas.width = 1910;
     int_canvas.height = 1910;
-    var tileSize = 100;
-    var x = 3;
-    var y = 3;
-
-    for(var tileX = 0; tileX < x; tileX ++) {
-        for(var tileY = 0; tileY < y; tileY++) {
-            ctx.drawImage(pic, tileX * tileSize,tileY * tileSize , tileSize, tileSize);
+    pic1.src = interior_pic; // Путь к изображению интерьера которое необходимо нанести на холст
+    pic2.src = wallpaper_pic; // Путь к изображению обоев которое необходимо нанести на холст
+    pic1.onload = function(){
+        if (interior_pic == '/media/BIANCA_foto1.png'){
+            // интерьер с зеркалом (крупный план)
+            fillWall(pic2, ctx, rapport, picSize=1.5);
+        } else if (interior_pic == '/media/foto4.png'){
+            // интерьер с двумя планами
+            fillWall(pic2, ctx, rapport, picSize=6);
+            fillWall(pic2, ctx, rapport, picSize=3, tileX=2.3);
+        } else if (interior_pic == '/media/interior1.png'){
+            // интерьер с желтым пуфиком
+            fillWall(pic2, ctx, rapport, picSize=5);
+        } else if (interior_pic == '/media/interior2.png'){
+            // интерьер с коричневым диваном
+            fillWall(pic2, ctx, rapport, picSize=5);
         }
+
+        ctx.drawImage(pic1, 0, 0); // устанавливаем фото интерьера
     }
 }
 
@@ -95,44 +80,45 @@ $.ajax({
 }
 
 
-// слайдеры
+//слайдеры//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// действие при клике на изображение обоев
 $(document).on('click', '.wall', function ()
 {
     $('#wallpaper-slider li').removeClass('active');
     $(this).parent().addClass('active');
-    $('#canvas_interior').css('background-image', 'url(' + $(this).children("img:first").attr('src') + ')');
     $('.wall img').css('padding', '0px');
     $(this).children('img').css('padding', '5px');
-
+    wallpaper_pic = $(this).children("img:first").attr('src');
+    showInterior(interior_pic, wallpaper_pic);
 });
 
+// действие при клике на изображение интерьера
 $(document).on('click', '.room', function ()
 {
     $('#interior_img').attr('src', $(this).children("img:first").attr('src'));
     $('.room img').css('padding', '0px');
     $(this).children('img').css('padding', '5px');
-
-    var interior_pic = $(this).children("img:first").attr('src');
-//    var wallpaper_pic = $('#wallpaper-slider .active img').attr('src');
-    showInterior(interior_pic); // при клике устанавливает выбранный интерьер
+    interior_pic = $(this).children("img:first").attr('src');
+    showInterior(interior_pic, wallpaper_pic); // при клике устанавливает выбранный интерьер
 });
 
+// действие при клике на изображение коллекции
 $('#collection-slider button').click(function() // из интерьеров в просмотр иколлекции
 {
     document.location.href='/collections/' + $(this).attr('id');
 });
 
 
-
+//действия при загрузке страницы с интерьером///////////////////////////////////////////////////////////////////////////
 
 $(document).ready(function() {
     // состояние кнопок
     $('#interior-slider li:first img').css('padding', '5px'); // выделяем кнопку интерьера по умолчанию
-    var interior_pic = $('#interior-slider li:first img').attr('src');
-    showInterior(interior_pic); // устанавливаем интерьер по умолчанию
-//    fillWall('/media/32608.jpg'); // заполнение стены интерьера обоями
+    showInterior(interior_pic, wallpaper_pic); // устанавливаем интерьер по умолчанию
 
-    // карусель
+
+    // слайдер для обоев
     var wallpaper_slider = $("#wallpaper-slider").lightSlider({
         autoWidth:true,
         loop:false,
@@ -141,6 +127,7 @@ $(document).ready(function() {
         slideMargin:0,
     });
 
+    // слайдер для коллекций
     var collection_slider = $('#collection-slider').lightSlider({
         item:1,
         mode:'fade',
@@ -153,6 +140,7 @@ $(document).ready(function() {
         },
     });
 
+    // слайдер для интерьеров
     $('#interior-slider').lightSlider({
         item:3,
         adaptiveHeight:true,
@@ -178,7 +166,7 @@ $(document).ready(function() {
         ]
     });
 
-    // перелистывает на нужную коллекцию при переходе со страницы предросмотра
+    // берет информацию о коллекции из предыдущего url для отображения на странице интерьера
     if (window.location.pathname == '/collections/interiors'){
         var oldURL = document.referrer.split("/")[4];
         if (oldURL){
